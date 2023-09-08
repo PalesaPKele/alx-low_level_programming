e630cfd20598a1bfb90c6a4d64f3aa3880c5a53a
PalesaPKele <palesakele@gmail.com>
2023-09-08 14:22:47 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -  program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * Return: 1 (success)
+ */
+int main(void)
+{
+fprintf("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+return (1);
+}
